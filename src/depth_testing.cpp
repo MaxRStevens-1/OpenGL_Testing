82c9d9a9a04aea38f7e43ae99da172bee8f1289d
Maxwell <maxwstevens1@gmail.com>
2025-05-19 21:38:08 -0400

    instanced arrays

--- src/depth_testing.cpp
+++ src/depth_testing.cpp
@@ -79,7 +79,7 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    Shader shader("instancing");
+    Shader shader("instancing_arrays");
     Shader skybox_shader("cubemap");
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -222,20 +222,6 @@ int main()
         0.05f, -0.05f,  0.0f, 1.0f, 0.0f,   
         0.05f,  0.05f,  0.0f, 1.0f, 1.0f		    		
     };  
-
-
-    unsigned int quadVAO, quadVBO;
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glBindVertexArray(quadVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_verticies), &quad_verticies, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-    glBindVertexArray(0);
-
     // setting the translations 2 pass 2 gpu
     glm::vec2 translations[100];
     int index = 0;
@@ -249,11 +235,37 @@ int main()
         }
     }  
 
+
+    // vao & vbo set up
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad_verticies), &quad_verticies, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    // setting up instancing vbo  
+
+    unsigned int instanceVBO;
+    glGenBuffers(1, &instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);	
+    glVertexAttribDivisor(2, 1);  
+    glBindVertexArray(0);
+
     // sending translations 2 vert shade
-    shader.use();
-    for(unsigned int i = 0; i < 100; i++) {
-        shader.setVec2(("offsets[" + std::to_string(i) + "]"), translations[i]);
-    }  
+    // shader.use();
+    // for(unsigned int i = 0; i < 100; i++) {
+    //     shader.setVec2(("offsets[" + std::to_string(i) + "]"), translations[i]);
+    // }  
+
+
 
     // load cubemap
     // -------------
